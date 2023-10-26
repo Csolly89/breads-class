ab316324f543e425518a89110b0a091e7e7ae643
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-10-25 20:41:44 -0400

    create and new routes complete

--- views/index.jsx
+++ views/index.jsx
@@ -5,19 +5,18 @@ function Index ({breads,title})  {
     return (
         <Default title={title}>
         <h2>Index Page</h2>
-        {/* <p>I have {breads[0].name} bread!</p> */}
-        {/* This is a JSX comment. */}
         <ul>
         {
             breads.map((bread, index)=> {
-                return (<li key={index}>
-                    <a href={`/breads/${index}`}>
-                    {bread.name}
-                    </a>
+                return (<li key={index}><a href={`/breads/${index}`}>{bread.name}</a>
                 </li>)
             })
         }
         </ul>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
       </Default>
       
     )
