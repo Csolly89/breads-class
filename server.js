07ab8145333495a441a647357bdff90640e43a2c
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-10-28 11:05:00 -0400

    delete route added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const methodOverride = require('method-override')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -7,21 +8,22 @@ const PORT = process.env.PORT
 const app = express()
 
 
-  // MIDDLEWARE
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'jsx')
-  app.engine('jsx', require('express-react-views').createEngine())
-  app.use(express.static('public'))
-  app.use(express.urlencoded({extended: true}))
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
 
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
 
 //404 page
 app.get('*', (req,res) => {
--- views/show.jsx
+++ views/show.jsx
@@ -1,19 +1,19 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
-      return (
-        <Default>
-  <h3>{bread.name}</h3>
-  <p>
-    this is {bread.hasGluten ? "not" : " "} gluten-free.
-  </p>
-  <img src={bread.image} alt={bread.name} />
-  <li><a href="/breads">Go home</a></li>
-</Default>
-
+function Show ({bread, index}) {
+  return (
+    <Default>
+      <h3>{bread.name}</h3>
+      <p>
+        this is {bread.hasGluten ? "not" : " "} gluten-free.
+      </p>
+      <img src={bread.image} alt={bread.name} />
+      <li><a href="/breads">Go home</a></li>
+      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+        <input type='submit' value="DELETE"/>
+      </form>
+    </Default>
       )
   }
   
